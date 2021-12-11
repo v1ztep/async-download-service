ff395df33218d328b5794235a8d747b8504fbf54
v1ztep <v1ztep@gmail.com>
2021-12-11 05:17:00 +0300

    Убрал хранение всего архива в памяти до завершения

--- main.py
+++ main.py
@@ -3,19 +3,16 @@ import asyncio
 
 async def archivate(folder='photos/', filename='archive.zip'):
     zip_args = f'-r - {folder}' # экранировать https://docs.python.org/3.7/library/shlex.html#shlex.quote
-    with open(filename, "w+b") as file:
-        bytes = bytearray(file.read())
-        subprocess = await asyncio.create_subprocess_shell(
-            f'zip {zip_args}',
-            stdout=asyncio.subprocess.PIPE
-        )
-        while True:
+    subprocess = await asyncio.create_subprocess_shell(
+        f'zip {zip_args}',
+        stdout=asyncio.subprocess.PIPE
+    )
+    while True:
+        with open(filename, "a+b") as file:
             stdout = await subprocess.stdout.read(n=512000)
-            bytes.extend(stdout)
-            file.seek(0)
-            file.write(bytes)
-            if subprocess.stdout.at_eof():
-                break
+            file.write(stdout)
+        if subprocess.stdout.at_eof():
+            break
 
 
 def main():
