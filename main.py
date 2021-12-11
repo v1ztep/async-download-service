5bc1ed695b6893f343c343b6b515f26d2b37529f
v1ztep <v1ztep@gmail.com>
2021-12-11 04:14:58 +0300

    Переписал архивацию на asyncio

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-import subprocess
+import asyncio
+
+
+async def archivate(folder='photos/', filename='archive.zip'):
+    zip_args = f'-r - {folder}' # экранировать https://docs.python.org/3.7/library/shlex.html#shlex.quote
+    with open(filename, "w+b") as file:
+        bytes = bytearray(file.read())
+        subprocess = await asyncio.create_subprocess_shell(
+            f'zip {zip_args}',
+            stdout=asyncio.subprocess.PIPE
+        )
+        while True:
+            stdout = await subprocess.stdout.read(n=512000)
+            bytes.extend(stdout)
+            file.seek(0)
+            file.write(bytes)
+            if subprocess.stdout.at_eof():
+                break
 
 
 def main():
-    zip_args = ['zip', '-r', '-', 'photos/']
-    with open('archive.zip', "w+b") as file:
-        bytes = bytearray(file.read())
-        bytes.extend(subprocess.check_output(zip_args))
-        file.seek(0)
-        file.write(bytes)
+    asyncio.run(archivate())
 
 
 if __name__ == '__main__':
