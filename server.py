17931c1574fc71d20b9b2c489f7a9cf25f6f9fae
v1ztep <v1ztep@gmail.com>
2021-12-15 19:56:46 +0300

    Остановил архивацию при обрыве соединения

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ import logging
 import os
 
 import aiofiles
-import aiohttp.web
 from aiohttp import web
 
 
@@ -11,7 +10,7 @@ async def archivate(request):
     zip_args = ['zip', '-r', '-', '.']
     folder = request.match_info.get('archive_hash')
     if not os.path.exists(f'photos/{folder}'):
-        raise aiohttp.web.HTTPNotFound(
+        raise web.HTTPNotFound(
             text='Архив не существует или был удален'
         )
     response = web.StreamResponse()
@@ -26,13 +25,26 @@ async def archivate(request):
         stderr=asyncio.subprocess.PIPE
     )
 
-    while True:
-        stdout = await subprocess.stdout.read(n=512000)
-        logging.debug(u'Sending archive chunk ...')
-        if subprocess.stdout.at_eof():
-            return response
-
-        await response.write(stdout)
+    try:
+        while True:
+            stdout = await subprocess.stdout.read(n=512000)
+            logging.debug('Sending archive chunk ...')
+            if subprocess.stdout.at_eof():
+                return response
+            await response.write(stdout)
+    except asyncio.CancelledError:
+        logging.error('Download was interrupted')
+        raise
+    except SystemExit as e:
+        logging.error(f'SystemExit error: {e}')
+        raise
+    except Exception as exception:
+        logging.error(f'Unexpected Exceptions: {exception}')
+        raise
+    finally:
+        if subprocess.returncode is None:
+            subprocess.kill()
+        return response
 
 
 async def handle_index_page(request):
