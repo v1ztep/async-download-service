e0b5583851b09f5f45090e533a61541be0fdace6
v1ztep <v1ztep@gmail.com>
2021-12-16 23:42:16 +0300

    Добавил аргументы

--- server.py
+++ server.py
@@ -1,15 +1,31 @@
+import argparse
 import asyncio
 import logging
 import os
+from functools import partial
 
 import aiofiles
 from aiohttp import web
 
 
-async def archivate(request):
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Микросервис для скачивания файлов'
+    )
+
+    parser.add_argument('--turn_off_logs', type=bool, default=False,
+                        help='Выключить логирование: default=False')
+    parser.add_argument('--delayed_response', type=int, default=0,
+                        help='Задержка ответа в секундах: default=0')
+    parser.add_argument('--dest_folder', type=str, default='photos',
+                        help='Путь к каталогу с фотографиями: default=photos')
+    return parser.parse_args()
+
+
+async def archivate(request, delayed_response, dest_folder):
     zip_args = ['zip', '-r', '-', '.']
     folder = request.match_info.get('archive_hash')
-    if not os.path.exists(f'photos/{folder}'):
+    if not os.path.exists(f'{dest_folder}/{folder}'):
         raise web.HTTPNotFound(
             text='Архив не существует или был удален'
         )
@@ -20,7 +36,7 @@ async def archivate(request):
 
     subprocess = await asyncio.create_subprocess_exec(
         *zip_args,
-        cwd=f'photos/{folder}',
+        cwd=f'{dest_folder}/{folder}',
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE
     )
@@ -32,6 +48,8 @@ async def archivate(request):
             if subprocess.stdout.at_eof():
                 return response
             await response.write(stdout)
+            if delayed_response:
+                await asyncio.sleep(delayed_response)
     except asyncio.CancelledError:
         logging.error('Download was interrupted')
         raise
@@ -57,10 +75,20 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
+    args = get_args()
     logging.basicConfig(level=logging.INFO)
+    if args.turn_off_logs:
+        logging.disable(logging.CRITICAL)
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archivate),
+        web.get(
+            '/archive/{archive_hash}/',
+            partial(
+                archivate,
+                delayed_response=args.delayed_response,
+                dest_folder=args.dest_folder
+            )
+        ),
     ])
     web.run_app(app)
